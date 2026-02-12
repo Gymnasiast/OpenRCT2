8771720f26ef712bfcbe45a1798496485bbe8cf3
Aaron van Geffen <aaron@aaronweb.net>
2026-02-12 17:07:15 +0100

    Update to miniaudio v0.11.24; address warnings

--- src/openrct2-ui/audio/MiniAudio.cpp
+++ src/openrct2-ui/audio/MiniAudio.cpp
@@ -3,6 +3,8 @@
 #pragma GCC diagnostic push 
 #pragma GCC diagnostic ignored "-Wold-style-cast"
 #pragma GCC diagnostic ignored "-Wmissing-declarations"
-#pragma GCC diagnostic ignored "-Wduplicated-branches"
+#pragma GCC diagnostic ignored "-Wmissing-prototypes"
+#pragma GCC diagnostic ignored "-Wmissing-variable-declarations"
+// #pragma GCC diagnostic ignored "-Wduplicated-branches"
 #include <thirdparty/miniaudio.h>
 #pragma GCC diagnostic pop
